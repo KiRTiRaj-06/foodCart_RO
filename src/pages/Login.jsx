0bc4fc682e20ba67ff98609793ea86f0d04b8c7e
divyaa1101 <divyaekhande01@gmail.com>
2026-04-07 15:25:48 +0530

    Add files via upload

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+function Login() {
+  return (
+    <div className="page">
+      <h1>Login</h1>
+
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
